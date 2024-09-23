7e3b7f6db1ddfcb58abe85851f0b0874b2b17cbd
LeilaAimetova15 <leilochkazai@gmail.com>
2024-09-23 11:25:34 +0300

    Update module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -1,6 +1,7 @@
 immutable_var = (1, 2, 3, 'Hello', 5.5)
 print(immutable_var)
 #immutable_var[0] = 15
+#Элементы кортежа неизменяемы, так как кортеж – это неизменяемая упорядоченная коллекция, которая может содержать в себе разные типы данных.
 mutable_list = ([5, 10], 'a', 'b', 3.2)
 mutable_list[0][0] = 25
 print(mutable_list)
